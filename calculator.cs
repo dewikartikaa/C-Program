d75f16973eb978e071998df38c2fee1dc3969ada
Dewi Kartika <48253396+dewikartikaa@users.noreply.github.com>
2019-03-14 21:02:46 +0700

    Add files via upload

---
+++ calculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            float result = 0;
+            
+            Console.WriteLine("Masukan angka pertama");
+            float angka1 = float.Parse(Console.ReadLine());
+
+            
+            Console.WriteLine("Masukan angka kedua");
+            float angka2 = float.Parse(Console.ReadLine());
+
+            
+            Console.WriteLine("Masukan Operator (+,-,*./)");
+            string oper = Console.ReadLine();
+
+            switch (oper)
+            {
+
+                case "+":
+                    result = angka1 + angka2;
+                    break;
+                case "-":
+                    result = angka1 - angka2;
+                    break;
+                case "*":
+                    result = angka1 * angka2;
+                    break;
+                case "/":
+                    result = angka1 / angka2;
+                    break;
+               
+            }
+            Console.WriteLine("Jawaban = " + result);
+            Console.Write("Tekan sembarang untuk keluar...");
+            Console.ReadKey();
+        }
+    }
+    
+}
